ac381f8b89eb7087769ceaa646012b46cf371e9e
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-03-10 15:05:55 +0500

    Added test route

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 
 
-
+app.get('/', (req, res) => {
+    res.send({example: 'Hello there!'});
+})
 
 
 
 const port = process.env.PORT || 5000;
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`App started on port ${port}`);
-})
+});
