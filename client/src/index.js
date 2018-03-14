33b7b3c964b93e8aaf2b76a7c71c6ff5ec0eb82d
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-03-14 15:13:55 +0500

    Added redux state manager

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(()=>[], {}, applyMiddleware());
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
