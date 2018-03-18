274f77d3e6268d866fda8113b86bb42d67fe9a76
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-03-18 10:31:08 +0500

    Added production build script

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
@@ -23,6 +24,14 @@ app.use(passport.session());
 app.use(bodyParser.json());
 
 app.use('/auth',authRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
